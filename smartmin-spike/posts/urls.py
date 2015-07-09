f644a9d93ebbe3e410e69718d656194504d3fa82
katmutua <jkm.mutua@gmail.com>
2015-07-09 18:07:42 +0300

    remove comments update models

--- smartmin-spike/posts/urls.py
+++ smartmin-spike/posts/urls.py
@@ -2,8 +2,6 @@
 	Configuration urls
 """
 
-from django.conf.urls import include, patterns, url
 from .views import *
 
 urlpatterns = PostCRUDL().as_urlpatterns()
-
--- smartmin-spike/posts/views.py
+++ smartmin-spike/posts/views.py
@@ -2,5 +2,4 @@ from smartmin.views import *
 from .models import *
 
 class PostCRUDL(SmartCRUDL):
-    actions = ('create', 'update', 'list')
     model = Post
